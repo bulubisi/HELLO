a4ab4bcaca3de3e4248d40bf65d9ff941c41c737
bulubisi <76817255+bulubisi@users.noreply.github.com>
2022-05-22 12:30:56 +0530

    Create git.c

---
+++ git.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a;
+    char ch;
+    printf("Enter a number :\n");
+    scanf("%d",&a);
+    printf("Enter a charactor:\n");
+    scanf("%c",&ch);
+    printf("The Number is %d\n",a);
+    printf("The charactor is %c\n",ch);
+    return 0;
+}
